5acb01fa9a12cb7bccfdd7b4806740c9292de04b
Lyuba <malceva-lyuba@mail.ru>
2021-03-03 18:48:55 +0300

    first commit

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Доброе утро" : "Доброго дня";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Form(FlowersResponse guest)
+        {
+            // Нужно отправить данные заказа
+
+            // (ModelState.IsValid)
+                return View("Thanks", guest);
+           //else
+                // Обнаружена ошибка проверки достоверности
+              //return View();
+            //return View("Thanks", guest);
+        }
+
+
+        // [HttpGet]
+        public ViewResult Bouquets()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ WebApplication/Models/FlowersResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace WebApplication.Models
+{
+    public class FlowersResponse
+    {
+        [Required(ErrorMessage = "Пожалуйста, введите наименование товара")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите количество товара")]
+        public string Quantity { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите ФИО")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите номер телефона")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите email")]
+        public string Email { get; set; }
+
+
+        public bool WillAttend { get; set; }
+    }
+}
